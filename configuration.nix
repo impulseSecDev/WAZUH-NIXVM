9262f56fbfdc96961d43c187b88d84ec1560da0c
tim <timothy.harris0187@gmail.com>
2026-05-13 23:07:37 -0400

    updated networking in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -79,11 +79,11 @@
   networking = {
     interfaces.enp1s0 = {
       ipv4.addresses = [{
-        address = "192.168.122.14";
+        address = "10.20.30.12";
         prefixLength = 24;
       }];
     };
-    defaultGateway = "192.168.122.1";
+    defaultGateway = "10.20.30.1";
     nameservers = [ "1.1.1.1" "8.8.8.8" ];
   };
 
